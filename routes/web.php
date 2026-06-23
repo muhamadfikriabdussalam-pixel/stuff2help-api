11481dec2a471437106f6934e16dcf60eebb6d02
Muhamad Fikri Abdussalam <muhamad.fikriabdussalam@gmail.com>
2026-06-23 07:58:17 +0700

    migration route final

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/gas-migrasi', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
-    return "Mantap! Semua tabel sukses dibuat di Supabase.";
+Route::get('/', function () {
+    return view('welcome');
 });
