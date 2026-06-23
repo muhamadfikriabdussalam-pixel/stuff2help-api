664698f66fdef66eb2fad1b2720ace30864dac18
Muhamad Fikri Abdussalam <muhamad.fikriabdussalam@gmail.com>
2026-06-23 07:40:26 +0700

    add temporary migration route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/gas-migrasi', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return "Mantap! Semua tabel sukses dibuat di Supabase.";
 });
